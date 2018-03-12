ac9d3bf364de55bcdd7980c84ba2ef21eeba7377
mrfarmer777@gmail.com <mrfarmer777@gmail.com>
2018-03-12 23:02:33 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,28 @@
+var katzDeliLine=[];
+
+var takeANumber =function(katzDeliLine,newName){
+  katzDeliLine.push(newName);
+  var greeting = "Welcome, " + newName+". You are number "+ katzDeliLine.length+"in line."
+  return greeting;
+};
+
+var newServing=function(katzDeliLine){
+  if(katzDeliLine.length>0){
+    return katzDeliLine.unshift();
+  } else {
+    return 'There is nobody waiting to be served!';
+  }
+};
+
+var currentLine=function(katzDeliLine){
+  if(katzDeliLine.length>0){
+    var outString = "The line is currently";
+    for(let i=0;i<katzDeliLine.length;i++){
+      if(i===1){
+        outString=outString+": "+i+". "+katzDeliLine[i];
+      } else {
+        outString=outString+", "+i+". "+katzDeliLine[i];
+      }
+    }
+  }
+}
